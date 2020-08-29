c9a0f7b48b6486f741ad7e63dbc4949ab21fa903
Matt Cummins <18335242+DoctorBazooka@users.noreply.github.com>
2020-08-30 00:11:25 +0100

    Add Sun to Scene (#27)
    - Add `Sun` struct and added to `Scene`. `DirectionalLight` and `PointLight will be removed but are currently still in.
    - Flattened out the Graphics folder by moving everything in Primitives up a level.

--- serialiser.py
+++ serialiser.py
@@ -25,6 +25,13 @@ void Save(const std::string& file, std::shared_ptr<Scene> scene)
 """
 
 middle0 = """
+    const auto& sun = scene->GetSun();
+    out << YAML::Key << "Sun" << YAML::BeginMap;
+    out << YAML::Key << "direction" << YAML::Value << sun.direction;
+    out << YAML::Key << "colour" << YAML::Value << sun.colour;
+    out << YAML::Key << "brightness" << YAML::Value << sun.brightness;
+    out << YAML::EndMap;
+
     out << YAML::Key << "Entities" << YAML::BeginSeq;
     scene->All([&](Entity& entity) {
         if (entity.Has<TemporaryComponent>()) { return; }
@@ -52,6 +59,13 @@ void Load(const std::string& file, std::shared_ptr<Scene> scene)
 
     YAML::Node data = YAML::Load(sstream.str());
     UpdateScene(data);
+
+    if (auto sun = data["Sun"]) {
+        scene->GetSun().direction = sun["direction"] ? sun["direction"].as<Maths::vec3>() : Maths::vec3{0.0, -1.0, 0.0};
+        scene->GetSun().colour = sun["colour"] ? sun["colour"].as<Maths::vec3>() : Maths::vec3{1.0, 1.0, 1.0};
+        scene->GetSun().brightness = sun["brightness"] ? sun["brightness"].as<float>() : 1.0f;
+    }
+
     if (!data["Entities"]) {
         return; // TODO: Error checking
     }
@@ -77,6 +91,7 @@ footer = """
 void Copy(std::shared_ptr<Scene> source, std::shared_ptr<Scene> target)
 {
     target->Clear();
+    target->GetSun() = source->GetSun();
     source->All([&](Entity& entity) {
         Copy(target, entity);
     });
