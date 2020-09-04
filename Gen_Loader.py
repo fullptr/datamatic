d7a9e2b781aa7f324ad8700da72bd32da2cef111
DoctorBazooka <18335242+DoctorBazooka@users.noreply.github.com>
2020-09-04 21:30:00 +0100

    Datamatic rewrite wip

--- Gen_Loader.py
+++ Gen_Loader.py
@@ -3,69 +3,81 @@ import os.path as op
 
 from . import definitions
 
-def savable(attrs):
-    for attr in attrs:
-        if attr.get("Savable", True):
-            yield attr
+def get_attrs(comp, flags):
+    attrs = comp["Attributes"] 
+    if "SAVABLE" in flags:
+        return (attr for attr in attrs if attr.get("Savable", True))
+    else:
+        return attrs
 
-def get_save_impl(spec):
+
+def fill_attribute(attr, line):
+    line = line.replace("{{Attr.Name}}", attr["Name"])
+    line = line.replace("{{Attr.DisplayName}}", attr["DisplayName"])
+    line = line.replace("{{Attr.Type}}", attr["Type"])
+
+    line = line.replace(
+        "{{Attr.Default}}",
+        definitions.default_cpp_repr(attr["Type"], attr["Default"])
+    )
+
+    assert "{{Attr." not in line, line
+    return line
+
+
+def fill_component(comp, line):
+    line = line.replace("{{Comp.Name}}", comp["Name"])
+    line = line.replace("{{Comp.DisplayName}}", comp["DisplayName"])
+
+    line = line.replace(
+        "{{Comp.Scriptable}}",
+        "true" if comp.get("Scriptable", True) else "false"
+    )
+
+    assert "{{Comp." not in line, line
+    return line
+
+
+def process_block(spec, block, flags):
     out = ""
-    for component in spec["Components"]:
-        name = component["Name"]
-        attrs = component["Attributes"]
-        out += f"        if(entity.Has<{name}>()) {{\n"
-        out += f"            const auto& c = entity.Get<{name}>();\n"
-        out += f'            out << YAML::Key << "{name}" << YAML::BeginMap;\n'
-        for attr in savable(attrs):
-            attr_name = attr["Name"]
-            out += f'            out << YAML::Key << "{attr_name}" << YAML::Value << c.{attr_name};\n'
-        out += "            out << YAML::EndMap;\n"
-        out += "        }\n"
+    for comp in spec["Components"]:
+        for line in block:
+            line = fill_component(comp, line)
+
+            if "{{Attr." in line:
+                for attr in get_attrs(comp, flags):
+                    out += fill_attribute(attr, line) + "\n"
+            else:
+                out += line + "\n"
+
     return out
 
-def get_load_impl(spec):
-    out = ""
-    for component in spec["Components"]:
-        name = component["Name"]
-        attrs = component["Attributes"]
-        out += f'        if (auto spec = entity["{name}"]) {{\n'
-        out += f'            {name} c;\n'
-        for attr in attrs:
-            attr_name = attr["Name"]
-            attr_type = attr["Type"]
-            if attr.get("Savable", True):
-                if "Default" in attr:
-                    out += f'            c.{attr_name} = spec["{attr_name}"] ? spec["{attr_name}"].as<{attr_type}>() : {definitions.default_cpp_repr(attr_type, attr["Default"])};\n'
-                else:
-                    out += f'            c.{attr_name} = spec["{attr_name}"].as<{attr_type}>();\n'
-        out += f'            e.Add(c);\n'
-        out += "        }\n"
-    return out
+def generate(spec, src, dst):
+    
+    with open(src) as srcfile:
+        lines = srcfile.readlines()
 
-def get_copy_impl(spec):
-    out = ""
-    for component in spec["Components"]:
-        name = component["Name"]
-        out += f"    if (entity.Has<{name}>()) {{\n"
-        out += f"        e.Add<{name}>(entity.Get<{name}>());\n"
-        out += "    }\n"
-    return out
+    in_block = False
+    block = []
+    flags = set()
+    out = "// GENERATED FILE\n"
+    for line in lines:
+        line = line.rstrip()
 
-def generate(spec, output):
-    template_file = op.join(op.dirname(op.abspath(__file__)), "Loader.dm.cpp")
+        if in_block:
+            if line == "#endif":
+                out += process_block(spec, block, flags)
+                in_block = False
+                block = []
+                flags = set()
+            else:
+                block.append(line)
+        elif line.startswith("#ifdef DATAMATIC_BLOCK"):
+            assert not in_block
+            in_block = True
+            flags = set(line.split()[2:])
+        else:
+            out += line + "\n"
 
-    with open(template_file) as template:
-        out = template.read()
-
-    assert "#define TIME_STAMP" in out
-    assert "#define LOADER_SAVE_IMPL" in out
-    assert "#define LOADER_LOAD_IMPL" in out
-    assert "#define LOADER_COPY_IMPL" in out
-
-    out = out.replace("#define TIME_STAMP", f"// GENERATED FILE @ {datetime.now()}")
-    out = out.replace("#define LOADER_SAVE_IMPL", get_save_impl(spec))
-    out = out.replace("#define LOADER_LOAD_IMPL", get_load_impl(spec))
-    out = out.replace("#define LOADER_COPY_IMPL", get_copy_impl(spec))
-
-    with open(output, "w") as outfile:
-        outfile.write(out)
+    with open(dst, "w") as dstfile:
+        dstfile.write(out)
