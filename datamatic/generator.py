40f5f0f3bd756c3fa23be292703e1457df526914
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-05-29 14:43:57 +0100

    Find the name of the builtin class programmatically

--- datamatic/generator.py
+++ datamatic/generator.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from functools import partial
 
 from .api import Plugin
+from .builtin import Builtin
 
 
 TOKEN = re.compile(r"\{\{(.*?)\}\}")
@@ -35,7 +36,7 @@ def parse_token_string(raw_string: str) -> Token:
 
     namespace = tokens[0]
     if len(tokens) == 2:
-        plugin_name = "Builtin"
+        plugin_name = Builtin.__name__
         function_name = tokens[1]
     elif len(tokens) == 3:
         plugin_name = tokens[1]
