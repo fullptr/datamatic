2d477320a4724a34d8bc58b488412c48f0981393
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-05-31 13:01:29 +0100

    Remove copy of builtin plugin from api.py (#21)
    * There is currently two copies of the plugin class. The one in api.py should have been removed in the last PR.

--- datamatic/api.py
+++ datamatic/api.py
@@ -166,8 +166,14 @@ def _(typename, firsttype, secondtype, obj) -> str:
 @parse.register("std::multimap<{}, {}>")
 @parse.register("std::unordered_multimap<{}, {}>")
 def _(typename, keytype, valuetype, obj) -> str:
-    assert isinstance(obj, list)
-    rep = ", ".join(f"{{{parse(keytype, k)}, {parse(valuetype, v)}}}" for k, v in obj)
+    if isinstance(obj, list):
+        pairs = obj
+    elif isinstance(obj, dict):
+        pairs = obj.items()
+    else:
+        raise RuntimeError(f"Could not parse {obj} as {typename}")
+
+    rep = "' ".join(f"{{{parse(keytype, k)}, {parse(valuetype, v)}}}" for k, v in pairs)
     return f"{typename}{{{rep}}}"
 
 
@@ -257,60 +263,3 @@ def compattrmethod(method):
     method._Comp = None
     method._Attr = None
     return classmethod(method)
-
-
-# BUITLIN PLUGINS
-
-
-class builtin(Plugin):
-    """
-    The default plugin. When parsing a token to replace, if there are only two parameters,
-    this is the assumed plugin. Contains simple accessors and some other helpers.
-    """
-
-
-    # Accessors
-
-    @compattrmethod
-    def name(cls, obj):
-        return obj["name"]
-
-    @compattrmethod
-    def display_name(cls, obj):
-        return obj["display_name"]
-
-    @attrmethod
-    def type(cls, attr):
-        return attr["type"]
-
-    @attrmethod
-    def default(cls, attr):
-        return parse(attr["type"], attr["default"])
-
-
-    # Conditional helpers
-
-    @compmethod
-    def if_nth_else(cls, comp, args, spec):
-        [n, yes_token, no_token] = args
-        return yes_token if comp == spec["components"][int(n)] else no_token
-
-    @compmethod
-    def if_first(cls, comp, args, spec):
-        [token] = args
-        return cls.if_nth_else(comp, ["0", token, ""], spec)
-
-    @compmethod
-    def if_not_first(cls, comp, args, spec):
-        [token] = args
-        return cls.if_nth_else(comp, ["0", "", token], spec)
-
-    @compmethod
-    def if_last(cls, comp, args, spec):
-        [token] = args
-        return cls.if_nth_else(comp, ["-1", token, ""], spec)
-
-    @compmethod
-    def if_not_last(cls, comp, args, spec):
-        [token] = args
-        return cls.if_nth_else(comp, ["-1", "", token], spec)
