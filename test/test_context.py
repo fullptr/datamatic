490670bc24b203c9d0df3fb00654339c1b8852fe
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-05-31 22:29:58 +0100

    Use ctx.parse rather than ctx.types.parse in context unit tests

--- test/test_context.py
+++ test/test_context.py
@@ -12,29 +12,29 @@ def ctx():
 
 
 def test_parse_int(ctx):
-    assert ctx.types.parse("int", 5) == "5"
+    assert ctx.parse("int", 5) == "5"
 
 
 def test_parse_float(ctx):
-    assert ctx.types.parse("float", 5) == "5.0f"
-    assert ctx.types.parse("float", 2.0) == "2.0f"
+    assert ctx.parse("float", 5) == "5.0f"
+    assert ctx.parse("float", 2.0) == "2.0f"
 
 
 def test_parse_double(ctx):
-    assert ctx.types.parse("double", 5) == "5.0"
-    assert ctx.types.parse("double", 2.0) == "2.0"
+    assert ctx.parse("double", 5) == "5.0"
+    assert ctx.parse("double", 2.0) == "2.0"
 
 
 def test_parse_bool(ctx):
-    assert ctx.types.parse("bool", True) == "true"
-    assert ctx.types.parse("bool", False) == "false"
+    assert ctx.parse("bool", True) == "true"
+    assert ctx.parse("bool", False) == "false"
 
     with pytest.raises(Exception):
-        ctx.types.parse("bool", 1)  # No implicit conversion
+        ctx.parse("bool", 1)  # No implicit conversion
 
 
 def test_parse_string(ctx):
-    assert ctx.types.parse("std::string", "Hello") == '"Hello"'
+    assert ctx.parse("std::string", "Hello") == '"Hello"'
 
 
 @pytest.mark.parametrize("container", [
@@ -50,36 +50,36 @@ def test_parse_string(ctx):
     "std::unordered_multiset",
 ])
 def test_parse_sequence(ctx, container):
-    assert ctx.types.parse(f"{container}<int>", [1, 2, 3, 4]) == f"{container}<int>{{1, 2, 3, 4}}"
+    assert ctx.parse(f"{container}<int>", [1, 2, 3, 4]) == f"{container}<int>{{1, 2, 3, 4}}"
 
     with pytest.raises(Exception):
-        ctx.types.parse(f"{container}<int>", 1)  # Object must be a list
+        ctx.parse(f"{container}<int>", 1)  # Object must be a list
 
     with pytest.raises(Exception):
-        ctx.types.parse(f"{container}<int>", ["a", "b"])  # Object must be a list of ints
+        ctx.parse(f"{container}<int>", ["a", "b"])  # Object must be a list of ints
 
 
 def test_parse_array(ctx):
-    assert ctx.types.parse("std::array<int, 5>", [1, 2, 3, 4, 5]) == "std::array<int, 5>{1, 2, 3, 4, 5}"
+    assert ctx.parse("std::array<int, 5>", [1, 2, 3, 4, 5]) == "std::array<int, 5>{1, 2, 3, 4, 5}"
 
     with pytest.raises(Exception):
-        ctx.types.parse("std::array<int, 4>", [1, 2, 3, 4, 5])  # Not enough elements
+        ctx.parse("std::array<int, 4>", [1, 2, 3, 4, 5])  # Not enough elements
 
     with pytest.raises(Exception):
-        ctx.types.parse("std::array<int, 6>", [1, 2, 3, 4, 5])  # Too many elements
+        ctx.parse("std::array<int, 6>", [1, 2, 3, 4, 5])  # Too many elements
 
 
 def test_parse_pair(ctx):
-    assert ctx.types.parse("std::pair<int, float>", [2, 3.0]) == "std::pair<int, float>{2, 3.0f}"
+    assert ctx.parse("std::pair<int, float>", [2, 3.0]) == "std::pair<int, float>{2, 3.0f}"
 
     with pytest.raises(Exception):
-        ctx.types.parse("std::pair<int, float>", 2)  # Must be a list
+        ctx.parse("std::pair<int, float>", 2)  # Must be a list
 
     with pytest.raises(Exception):
-        ctx.types.parse("std::pair<int, float>", [2, 3.0, 1])  # Must be a list of two elements
+        ctx.parse("std::pair<int, float>", [2, 3.0, 1])  # Must be a list of two elements
 
     with pytest.raises(Exception):
-        ctx.types.parse("std::pair<int, float>", [2, "a"])  # Element types must match
+        ctx.parse("std::pair<int, float>", [2, "a"])  # Element types must match
 
 
 @pytest.mark.parametrize("container", [
@@ -92,66 +92,66 @@ def test_parse_mapping(ctx, container):
     expected = f"{container}<int, int>{{{{1, 2}}, {{3, 4}}}}"
 
     # List of pairs JSON object
-    assert ctx.types.parse(f"{container}<int, int>", [[1, 2], [3, 4]]) == expected
+    assert ctx.parse(f"{container}<int, int>", [[1, 2], [3, 4]]) == expected
 
     # Dict JSON object
-    assert ctx.types.parse(f"{container}<int, int>", {1: 2, 3: 4}) == expected
+    assert ctx.parse(f"{container}<int, int>", {1: 2, 3: 4}) == expected
 
     with pytest.raises(Exception):
-        ctx.types.parse(f"{container}<int, int>", 1)  # Object must be a list or dict
+        ctx.parse(f"{container}<int, int>", 1)  # Object must be a list or dict
 
 
 def test_parse_optional(ctx):
-    assert ctx.types.parse("std::optional<int>", 5) == "std::optional<int>{5}"
-    assert ctx.types.parse("std::optional<int>", None) == "std::nullopt"
+    assert ctx.parse("std::optional<int>", 5) == "std::optional<int>{5}"
+    assert ctx.parse("std::optional<int>", None) == "std::nullopt"
 
 
 @pytest.mark.parametrize("ptr_type", ["unique", "shared"])
 def test_parse_smart_pointer(ctx, ptr_type):
-    assert ctx.types.parse(f"std::{ptr_type}_ptr<int>", 5) == f"std::make_{ptr_type}<int>(5)"
-    assert ctx.types.parse(f"std::{ptr_type}_ptr<int>", None) == "nullptr"
+    assert ctx.parse(f"std::{ptr_type}_ptr<int>", 5) == f"std::make_{ptr_type}<int>(5)"
+    assert ctx.parse(f"std::{ptr_type}_ptr<int>", None) == "nullptr"
 
 
 def test_parse_weak_pointer(ctx):
-    assert ctx.types.parse("std::weak_ptr<int>", None) == "nullptr"
+    assert ctx.parse("std::weak_ptr<int>", None) == "nullptr"
     
     with pytest.raises(Exception):
-        ctx.types.parse("std::weak_ptr<int>", 4)  # weak pointers can only be initialised to nullptr
+        ctx.parse("std::weak_ptr<int>", 4)  # weak pointers can only be initialised to nullptr
 
 
 @pytest.mark.parametrize("typ", ["std::any", "std::monostate"])
 def test_parse_any_and_monostate(ctx, typ):
-    assert ctx.types.parse(typ, None) == f"{typ}{{}}"
+    assert ctx.parse(typ, None) == f"{typ}{{}}"
 
     with pytest.raises(Exception):
-        ctx.types.parse(typ, 5)  # Can only default initialise
+        ctx.parse(typ, 5)  # Can only default initialise
 
 
 def test_parse_tuple(ctx):
     tuple_type = "std::tuple<int, float, double>"
-    assert ctx.types.parse(tuple_type, [1, 1, 1]) == f"{tuple_type}{{1, 1.0f, 1.0}}"
+    assert ctx.parse(tuple_type, [1, 1, 1]) == f"{tuple_type}{{1, 1.0f, 1.0}}"
 
     with pytest.raises(Exception):
-        ctx.types.parse(tuple_type, [1, 2])  # Not enough elements
+        ctx.parse(tuple_type, [1, 2])  # Not enough elements
 
     with pytest.raises(Exception):
-        ctx.types.parse(tuple_type, ["a", "b", "c"])  # Type mismatch
+        ctx.parse(tuple_type, ["a", "b", "c"])  # Type mismatch
 
 
 def test_parse_variant(ctx):
     # Value is parsed as the first successful type
-    assert ctx.types.parse("std::variant<int, float>", 5) == "5"
-    assert ctx.types.parse("std::variant<float, int>", 5) == "5.0f"
-    assert ctx.types.parse("std::variant<int, std::string>", "Hello") == '"Hello"'
-    assert ctx.types.parse("std::variant<int, std::map<int, int>>", {1: 2}) == "std::map<int, int>{{1, 2}}"
+    assert ctx.parse("std::variant<int, float>", 5) == "5"
+    assert ctx.parse("std::variant<float, int>", 5) == "5.0f"
+    assert ctx.parse("std::variant<int, std::string>", "Hello") == '"Hello"'
+    assert ctx.parse("std::variant<int, std::map<int, int>>", {1: 2}) == "std::map<int, int>{{1, 2}}"
 
     with pytest.raises(Exception):
-        ctx.types.parse("std::variant<int, std::string>", None)  # Value does not parse as any subtype
+        ctx.parse("std::variant<int, std::string>", None)  # Value does not parse as any subtype
 
 
 def test_parse_function(ctx):
     # No checks occur on std::function
-    assert ctx.types.parse("std::function<int(bool)>", "a") == "a"
+    assert ctx.parse("std::function<int(bool)>", "a") == "a"
 
 
 def test_typeparser_unregistered_type():
@@ -160,7 +160,7 @@ def test_typeparser_unregistered_type():
 
     # THEN
     with pytest.raises(Exception):
-        ctx.types.parse("int", 4)  # Type is not registered
+        ctx.parse("int", 4)  # Type is not registered
 
 
 def test_typeparser_register():
@@ -173,7 +173,7 @@ def test_typeparser_register():
         return f"{typename}, {obj}"
 
     # THEN
-    assert ctx.types.parse("foo", None) == f"foo, None"
+    assert ctx.parse("foo", None) == f"foo, None"
 
 
 def test_typeparser_template_register():
@@ -186,7 +186,7 @@ def test_typeparser_template_register():
         return f"{typename}, {subtype}, {obj}"
 
     # THEN
-    assert ctx.types.parse("foo_int", None) == f"foo_int, int, None"
+    assert ctx.parse("foo_int", None) == f"foo_int, int, None"
 
 
 def test_typeparser_variadic_register():
@@ -200,7 +200,7 @@ def test_typeparser_variadic_register():
 
     # THEN
     expected = f"foo<int, float, int>, ['int', 'float', 'int'], None"
-    assert ctx.types.parse("foo<int, float, int>", None) == expected
+    assert ctx.parse("foo<int, float, int>", None) == expected
 
 
 def test_variadic_typelist_parser():
