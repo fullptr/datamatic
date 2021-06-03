cc17aec435a7f75f1bbd850a3055d3a83af50754
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-06-03 19:46:51 +0100

    Move the argument parser to the datamatic.py file

--- datamatic.py
+++ datamatic.py
@@ -1,8 +1,38 @@
 """
 A tool for generating code based on a schema of components.
 """
+import argparse
+import pathlib
 from datamatic import main
 
+
+def parse_args():
+    """
+    Read the command line.
+    """
+    parser = argparse.ArgumentParser(
+        description="Given a component spec and a directory, scan the "
+                    "directory for dm and dmx files and generate the "
+                    "appropriate files"
+    )
+
+    parser.add_argument(
+        "-s", "--spec",
+        required=True,
+        type=pathlib.Path,
+        help="A path to the component spec JSON file"
+    )
+
+    parser.add_argument(
+        "-d", "--dir",
+        required=True,
+        type=pathlib.Path,
+        help="A path to the directory to scan for dm and dmx files"
+    )
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    args = main.parse_args()
+    args = parse_args()
     main.main(args.spec, args.dir)
--- datamatic/main.py
+++ datamatic/main.py
@@ -28,33 +28,6 @@ def fill_flag_defaults(spec):
             attr["flags"] = {**defaults, **attr_flags}
 
 
-def parse_args():
-    """
-    Read the command line.
-    """
-    parser = argparse.ArgumentParser(
-        description="Given a component spec and a directory, scan the "
-                    "directory for dm and dmx files and generate the "
-                    "appropriate files"
-    )
-
-    parser.add_argument(
-        "-s", "--spec",
-        required=True,
-        type=pathlib.Path,
-        help="A path to the component spec JSON file"
-    )
-
-    parser.add_argument(
-        "-d", "--dir",
-        required=True,
-        type=pathlib.Path,
-        help="A path to the directory to scan for dm and dmx files"
-    )
-
-    return parser.parse_args()
-
-
 def main(specfile: pathlib.Path, directory: pathlib.Path):
     """
     Entry point.
