d07edef36aea94677cab06310b97eca506d62881
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-06-03 20:06:50 +0100

    Cover the case of not rewriting the file

--- datamatic/generator.py
+++ datamatic/generator.py
@@ -1,5 +1,5 @@
 import re
-import copy
+import pathlib
 from typing import Tuple, Literal, Optional
 from dataclasses import dataclass, field
 from functools import partial
--- test/integration/test_end_to_end.py
+++ test/integration/test_end_to_end.py
@@ -5,6 +5,7 @@ import tempfile
 import shutil
 import pathlib
 import os.path as op
+from unittest.mock import patch, call
 from datamatic import main
 
 
@@ -47,3 +48,37 @@ def test_end_to_end():
     actual_file = op.join(out_dir, "actual.cpp")
     with open(expected_file) as expected, open(actual_file) as actual:
         assert expected.read() == actual.read()
+
+
+def test_file_is_not_rewritten_if_no_change():
+    """
+    Similar to the above test, except in this one we also copy the expected file into the
+    output directory. The file should not be written. We check this by patching
+    print and making sure the expected message was printed. This is a bit brittle, so maybe
+    there's a better way of doing this.
+    """
+
+    # GIVEN
+    # The directory that this file is in. The template files are also located here.
+    src_dir = op.dirname(op.abspath(__file__))
+
+    # Create a new directory to run the test in.
+    out_dir = tempfile.mkdtemp(prefix="datamatic_")
+    
+    # Copy the template file into the output and verify it's there. Also copy the expected.
+    shutil.copy(op.join(src_dir, "actual.dm.cpp"), op.join(out_dir, "actual.dm.cpp"))
+    assert op.exists(op.join(out_dir, "actual.dm.cpp"))
+
+    shutil.copy(op.join(src_dir, "custom_types.dmx.py"), op.join(out_dir, "custom_types.dmx.py"))
+    assert op.exists(op.join(out_dir, "custom_types.dmx.py"))
+
+    shutil.copy(op.join(src_dir, "expected.cpp"), op.join(out_dir, "actual.cpp"))
+    assert op.exists(op.join(out_dir, "actual.cpp"))
+
+    # WHEN
+    specfile = pathlib.Path(src_dir, "component_spec.json")
+    with patch("builtins.print") as mock_print:
+        main.main(specfile, pathlib.Path(out_dir))
+
+    # THEN
+    mock_print.assert_any_call(f"No change to {op.join(out_dir, 'actual.cpp')}")
