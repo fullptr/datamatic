14b79cc06dbe8ddaed94a30306de00b7b5c91414
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-06-06 02:11:53 +0100

    Simplify down the integration tests

--- test/integration/test_end_to_end.py
+++ test/integration/test_end_to_end.py
@@ -1,77 +1,71 @@
 """
 An integration test that uses a specfile and a template file.
 """
-import tempfile
 import shutil
-import pathlib
+from pathlib import Path
 import os.path as op
 from unittest.mock import patch
 from datamatic import main
+import pytest
 
 
 # The directory that this file is in. The template files are also located here.
-src_dir = op.dirname(op.abspath(__file__))
+@pytest.fixture
+def src_path():
+    return Path(op.dirname(op.abspath(__file__)))
 
 
-def copy_to_test(src_dir, out_dir, filename, new_filename=None):
+def copy_to_test(src_dir: Path, out_dir: Path, filename, new_filename=None):
     if new_filename is None:
         new_filename = filename
-    shutil.copy(op.join(src_dir, filename), op.join(out_dir, new_filename))
-    assert op.exists(op.join(out_dir, new_filename))
+
+    old_file = src_dir / filename
+    new_file = out_dir / new_filename
+
+    shutil.copy(str(old_file), str(new_file))
+    assert new_file.exists()
 
 
-def test_end_to_end_simple():
+def test_end_to_end_simple(src_path, tmp_path):
     """
     Creates a temporary directory and copies the template file. Run datamatic on the directory
     and verify that a new file as been created with the same contents as the expected file.
 
     This end_to_end test should cover all datamatic features, such as flags, custom types and
     custom functions.
-    """
-
-    # GIVEN
-    # Create a new directory to run the test in.
-    out_dir = tempfile.mkdtemp(prefix="datamatic_")
-    
+    """    
     # Copy the template file into the output and verify it's there
-    copy_to_test(src_dir, out_dir, "actual.dm.cpp")
-    copy_to_test(src_dir, out_dir, "custom_functions.dmx.py")
+    copy_to_test(src_path, tmp_path, "actual.dm.cpp")
+    copy_to_test(src_path, tmp_path, "custom_functions.dmx.py")
 
     # WHEN
-    specfile = pathlib.Path(src_dir, "component_spec.json")
-    main.main(specfile, pathlib.Path(out_dir))
+    specfile = src_path / "component_spec.json"
+    main.main(specfile, tmp_path)
 
-    # THEN
-    # New file should be created
-    assert op.exists(op.join(out_dir, "actual.cpp"))
-
-    expected_file = op.join(src_dir, "expected.cpp")
-    actual_file = op.join(out_dir, "actual.cpp")
+    expected_file = src_path / "expected.cpp"
+    actual_file = tmp_path / "actual.cpp"
+    
+    assert actual_file.exists()
     with open(expected_file) as expected, open(actual_file) as actual:
         assert expected.read() == actual.read()
 
 
-def test_file_is_not_rewritten_if_no_change():
+def test_file_is_not_rewritten_if_no_change(src_path, tmp_path):
     """
     Similar to the above test, except in this one we also copy the expected file into the
     output directory. The file should not be written. We check this by patching
     print and making sure the expected message was printed. This is a bit brittle, so maybe
     there's a better way of doing this.
     """
-
-    # GIVEN
-    # Create a new directory to run the test in.
-    out_dir = tempfile.mkdtemp(prefix="datamatic_")
-    
     # Copy the template file into the output and verify it's there. Also copy the expected.
-    copy_to_test(src_dir, out_dir, "actual.dm.cpp")
-    copy_to_test(src_dir, out_dir, "custom_functions.dmx.py")
-    copy_to_test(src_dir, out_dir, "expected.cpp", "actual.cpp")
+    copy_to_test(src_path, tmp_path, "actual.dm.cpp")
+    copy_to_test(src_path, tmp_path, "custom_functions.dmx.py")
+    copy_to_test(src_path, tmp_path, "expected.cpp", "actual.cpp")
 
     # WHEN
-    specfile = pathlib.Path(src_dir, "component_spec.json")
+    specfile = src_path / "component_spec.json"
     with patch("builtins.print") as mock_print:
-        main.main(specfile, pathlib.Path(out_dir))
+        main.main(specfile, tmp_path)
 
     # THEN
-    mock_print.assert_any_call(f"No change to {op.join(out_dir, 'actual.cpp')}")
+    mock_print.assert_any_call(f"No change to {tmp_path / 'actual.cpp'}")
