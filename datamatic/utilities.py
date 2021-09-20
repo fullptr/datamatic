460d4b390864628146fbf10ad0afe0a65faae895
Matt Cummins <18335242+MagicLemma@users.noreply.github.com>
2021-09-20 22:17:20 +0100

    Add docstring

--- datamatic/utilities.py
+++ datamatic/utilities.py
@@ -5,13 +5,17 @@ well as being made available to plugins.
 
 
 def flag_match(obj, flags):
+    """
+    Given a component or attribute, return true if it matches all of the given flags
+    and False otherwise.
+    """
     assert "flags" in obj
     return all(obj['flags'][key] == value for key, value in flags.items())
 
 
 def filter_flags(obj_list, flags):
     """
-    Given a list of components or attributes, and a dict of flags, yield all objects in
-    the list that match the flags.
+    Given a list of components or attributes, and a dict of flags, return a list containing
+    only the objects that match the flags.
     """
     return [obj for obj in obj_list if flag_match(obj, flags)]
